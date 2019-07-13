9c5ff8ba164152d669924da7f0b91088381392b2
Joel Jhonny Porras Caso <joel@landgorilla.com>
2019-07-13 12:30:01 -0500

    Update

---
+++ docker_test.sh
@@ -0,0 +1,15 @@
+docker rm -f rex
+
+docker run --name=rex -d -p 8181:8080 chasky/spring-calc:$1
+if [ $? != 0 ]; then
+  exit 1
+fi
+
+#until $(curl --output /dev/null --silent --head --fail http://localhost:8181/greeting?name=Rex); do
+#    printf '.'
+    sleep 5
+#done
+
+
+curl -I http://localhost:8181/greeting?name=Rex
+docker rm -f rex
